4bb3c09381f129b20b7dd77d3f388e6be2e143cb
ajaysharma12799 <ajaysharma12799@gmail.com>
2023-03-14 12:38:30 +0530

    Removed Unnecessary Code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FC } from 'react'
+import TodoContainer from './components/TodoApp/TodoContainer'
+import ApiContainer from './components/ApiCalling/ApiContainer'
 
-function App() {
+const App: FC = (): JSX.Element => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <TodoContainer /> */}
+      <ApiContainer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
